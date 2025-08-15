aea2cc835a2778eccbe78b5e7137b816d85fc30d
Andrii <astaro.balf@gmail.com>
2025-08-15 14:18:37 +0300

    pytest and fixture

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+class User:
+
+    def _init__(self) -> None:
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = "Andrii"
+        self.second_name = "Savchenko"
+
+    def remove(self):
+        self.name = "" 
+        self.second_name = ""
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user
+
+    user.remove()
