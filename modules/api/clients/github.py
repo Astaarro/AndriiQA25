fd48dc5602292dfa5d4e07db6b8e8f0e896d7011
Andrii <astaro.balf@gmail.com>
2025-08-28 16:20:46 +0300

    API Tests Homework

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -13,3 +13,30 @@ class Github:
         body = r.json()
 
         return body
+    
+
+#INDIVIDUAL TESTS
+
+    def emojis(self):
+        r = requests.get('https://api.github.com/emojis')
+        body = r.json()
+
+        return body
+    
+    def emojis_response(self):
+        response = requests.get("https://api.github.com/emojis")
+      
+        return response
+    
+
+    def commit(self, owner, repo):
+        r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits')
+        body = r.json()
+
+        return body
+    
+    def commit_responce(self, owner, repo):
+        response = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits')
+    
+        return response
+    
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -25,3 +25,44 @@ def test_repo_cannot_be_found(github_api):
 def test_with_cingle_char_be_found(github_api):
     r = github_api.search_repo('s')
     assert r['total_count'] != 0
+
+#INDIVIDUAL TESTS
+
+# Test1: Is the emoji "adult" included in the list?
+@pytest.mark.api
+def test_emojis(github_api):
+    r = github_api.emojis()
+    assert 'adult' in r
+    #print(r)
+
+# Test2: Is the unexist emoji "adult10"?
+@pytest.mark.api
+def test_unexist_emojis(github_api):
+    r = github_api.emojis()
+    assert 'adult10' not in r
+
+# Test3: Is the status_code emoji 200?
+@pytest.mark.api
+def test_status_code(github_api):
+    r = github_api.emojis_response()
+    assert r.status_code == 200
+
+
+#Test4: Is exist commit response 200?
+@pytest.mark.api
+def test_status_code_commit(github_api):
+    r = github_api.commit_responce('Astaarro', 'AndriiQA25')
+    assert r.status_code == 200
+
+#Test5: Is not exist commit response 404?
+@pytest.mark.api
+def test_status_code_unexist_commit(github_api):
+    r = github_api.commit_responce('Astaarro1111', 'AndriiQA25')
+    assert r.status_code == 404
+
+#Test6: Is user name Astaarro?
+@pytest.mark.api
+def test_user_name_in_commit(github_api):
+    commits = github_api.commit('Astaarro', 'AndriiQA25')
+    first_commit = commits[0]  
+    assert "Astaarro" in first_commit["commit"]["author"]["name"]
