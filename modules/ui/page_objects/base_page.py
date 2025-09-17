0c22f91c81b243570db7c7bf96984a57dda1fe88
Astaarro <astaro.balf@gmail.com>
2025-09-17 19:28:33 +0300

    bug_fix

--- modules/ui/page_objects/base_page.py
+++ modules/ui/page_objects/base_page.py
@@ -4,16 +4,37 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from conftest import driver
 
 class BasePage:
+
+ 
+    PATH = r"C:\Users\AMD\AndriiQA25\AndriiQA25"
+    DRIVER_NAME = "chromedriver"
+
+    
+
+
+    def close(self):
+        self.driver.close()
     """Базовий клас для об'єктів сторінок, який містить загальні методи та ініціалізацію драйвера."""
     
     # Локатор для іконки/посилання кошика в хедері
     CART_LINK = (By.CSS_SELECTOR, ".wd-header-cart a[href*='cart']")
 
-    def __init__(self, driver) -> None:
-        """Ініціалізує драйвер браузера або використовує існуючий."""
-        self.driver = driver
+  
+    def __init__(self, driver=None):
+        if driver is None:
+            options = Options()
+            options.add_argument("--start-maximized")
+            # webdriver-manager завантажує правильний драйвер
+            self.driver = webdriver.Chrome(
+                service=Service(ChromeDriverManager().install()),
+                options=options
+            )
+        else:
+            self.driver = driver
         
     def close(self):
         """Закриває вікно браузера."""
